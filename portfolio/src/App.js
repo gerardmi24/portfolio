08c89bd852fd22aee446b401c768efeacef60ff4
gerardmi24 <gerardmistretta@yahoo.com>
2021-04-03 15:40:17 -0400

    Portfolio Foundation

---
+++ portfolio/src/App.js
@@ -0,0 +1,31 @@
+import './App.css';
+import {Switch, Route} from 'react-router-dom'
+import Welcome from './Welcome'
+import Projects from './Projects'
+import Skills from './Skills'
+import Nav from './Nav'
+import Footer from './Footer'
+
+function App() {
+
+  return (
+    <div className="App">
+      <Switch>
+        <Route exact path="/">
+          <Welcome />
+          <Footer />
+        </Route>
+        <Route exact path="/projects">
+          <Nav />
+          <Projects />
+        </Route>
+        <Route exact path="/skills">
+          <Nav />
+          <Skills />
+        </Route>
+      </Switch>
+    </div>
+  );
+}
+
+export default App;
---
+++ portfolio/src/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Footer() {
+    return (
+        <div className='footsies'>
+            <h5>Contact | gerardmistretta@gmail.com</h5>
+        </div>
+    )
+}
