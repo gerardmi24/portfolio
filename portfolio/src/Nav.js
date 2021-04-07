a51d3cc5aa8c9f6208f00a7e940240bdc260a5ab
gerardmi24 <gerardmistretta@yahoo.com>
2021-04-06 22:27:52 -0400

    Nav done

--- portfolio/src/Nav.js
+++ portfolio/src/Nav.js
@@ -1,9 +1,31 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import Footer from './Footer'
 
 export default function Nav() {
     return (
         <div>
-            Da Nav
+            <div>
+                <h2>Gerard Mistretta</h2>
+                <br />
+                <br />
+                <p>Fullstack Software Engineer</p>
+                <br />
+            </div>
+            <div>
+                <a href="https://github.com/gerardmi24" target="_blank" rel="noreferrer noopener" ><i className="fab fa-github-square"></i></a>
+                <a href="https://www.linkedin.com/in/gerard-mistretta/" target="_blank" rel="noreferrer noopener"><i className="fab fa-linkedin"></i></a>
+                <a href="https://gerardmistretta.medium.com/" target="_blank" rel="noreferrer noopener"><i className="fab fab fa-medium"></i></a>
+            </div>
+            <div className="navLinks">
+                <li><Link to="/" className="link"> About </Link></li>
+                <li><Link to="/projects" className="link"> Projects </Link></li>
+                <li><Link to="/skills" className="link"> Skills </Link></li>
+                <li><a className="link"  target="_blank" rel="noreferrer noopener"href="https://docs.google.com/document/d/1Hf_4ZcvoPeXyfCt5hgiqUslGLuO2pHRJmiaZo7HYbx4/edit">Resume</a></li>
+            </div>
+            <div className="nav-footer">
+            <Footer />
+            </div>
         </div>
     )
 }
--- portfolio/src/Welcome.js
+++ portfolio/src/Welcome.js
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 import ProfPic from '/Users/gerardmistretta/Development/portfolio/portfolio/src/images/profilePic.jpeg';
 import GitLogo from '/Users/gerardmistretta/Development/portfolio/portfolio/src/images/GithubLogo.jpeg';
 import LinkedInLogo from '/Users/gerardmistretta/Development/portfolio/portfolio/src/images/LinkedInIcon.jpeg';
+import MediumIcon from '/Users/gerardmistretta/Development/portfolio/portfolio/src/images/MediumIcon.jpeg';
 
 export default function Welcome() {
     return (
@@ -13,22 +14,23 @@ export default function Welcome() {
                         <h1 id="headerTitle"> Welcome to my Portfolio!</h1>
                         <div>
                             <h2>Gerard Mistretta</h2>
-                            <h4>Full Stack Web Developer</h4>
+                            <h4>Fullstack Web Developer</h4>
                         </div>
                     </div>
                     <img src={GitLogo} />
                     <img src={LinkedInLogo} />
+                    <img src={MediumIcon} />
                     <img src={ProfPic} />
                     <div>
                         <Link className="navOpps" to="/projects">Projects</Link> 
                         <Link className="navOpps" to="/skills">Skills</Link>
-                        <a className="navOpps" target="_blank" rel="noreferrer noopener">Resume</a>
+                        <a className="navOpps" target="_blank" rel="noreferrer noopener" href="https://docs.google.com/document/d/1Hf_4ZcvoPeXyfCt5hgiqUslGLuO2pHRJmiaZo7HYbx4/edit">Resume</a>
                     </div>
                 </div>
             </header>
             <div>
                 <p>
-                    Hello and welcome! I am a Full Stack Software Engineer exploring the ever changing 
+                    Hello and welcome! I am a Fullstack Software Engineer exploring the ever changing 
                     landscape of technology. After earning my MBA and working in Marketing for a few years, 
                     I decided to take on a new challenge and enter the fascinating world of Web Development. 
                     After receiving my certification from Flatiron School's Software Engineering Bootcamp, 
